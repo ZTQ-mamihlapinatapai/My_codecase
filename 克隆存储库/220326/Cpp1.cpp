0f9a789f94f82a09cb61ddab07928decc1869076
ZTQ-mamihlapinatapai <102413299+ZTQ-mamihlapinatapai@users.noreply.github.com>
2022-05-21 17:09:22 +0800

    Add files via upload

---
+++ 克隆存储库/220326/Cpp1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>;
+#include <string>;
+using namespace std;
+int main()
+{
+	string str1="hello world";
+	char str2[]="hello world";
+	cout<<str1<<endl;
+	//short num1=10;
+	//cout<<"short所占内存空间为："<<sizeof(short)<<endl;
+	//system("pause");
+	return 0;
+}
+/*
+#include <iostream>
+using namespace std;
+#define Day 7
+int main()
+{
+	cout<<"一周有"<<Day<<"天"<< endl;
+	system("pause");
+	return 0;
+}
+*/
